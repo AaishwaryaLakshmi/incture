d2b21a8fa3a82448fbbac253ae2c132b96ec9d04
Aaishwarya Lakshmi <84765376+AaishwaryaLakshmi@users.noreply.github.com>
2024-02-19 08:04:47 +0530

    Create Header.js

---
+++ Wk06_AaishwaryaLakshmiP/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Foodie Mania</h1>
+    </header>
+  );
+};
+
+export default Header;
