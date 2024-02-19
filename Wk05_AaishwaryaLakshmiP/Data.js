737c2ecba85cdedd3e78cde20fe118b71e08d444
Aaishwarya Lakshmi <84765376+AaishwaryaLakshmi@users.noreply.github.com>
2024-02-19 07:59:38 +0530

    Create Data.js

---
+++ Wk05_AaishwaryaLakshmiP/Data.js
@@ -0,0 +1,40 @@
+// Data.js
+export const dummyRestaurants = [
+    {
+      name: 'The Hungry Hippo',
+      address: '123 Main St',
+      menu: [
+        { name: 'Pizza', price: 10, quantity: 5 },
+        { name: 'Burger', price: 8, quantity: 8 },
+        { name: 'Wraps', price: 12, quantity: 10 },
+      ],
+    },
+    {
+      name: 'Bella Italia',
+      address: '156 Main St',
+      menu: [
+        { name: 'Sushi', price: 15, quantity: 10 },
+        { name: 'Pasta', price: 12, quantity: 7 },
+        { name: 'Burger', price: 10, quantity: 8 },
+      ],
+    },
+    {
+      name: 'Tasty Tacos',
+      address: '789 Elm St',
+      menu: [
+        { name: 'Tacos', price: 8, quantity: 6 },
+        { name: 'Burritos', price: 9, quantity: 5 },
+        { name: 'Quesadillas', price: 7, quantity: 8 },
+      ],
+    },
+    {
+      name: 'Sushi Palace',
+      address: '246 Oak St',
+      menu: [
+        { name: 'Sashimi', price: 20, quantity: 6 },
+        { name: 'Maki Rolls', price: 15, quantity: 8 },
+        { name: 'Nigiri', price: 18, quantity: 10 },
+      ],
+    },
+  ];
+  
