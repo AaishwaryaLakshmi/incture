a04c9ef459f2ce8bed07a0fde3b57241b0534355
Aaishwarya Lakshmi <84765376+AaishwaryaLakshmi@users.noreply.github.com>
2024-02-19 08:01:28 +0530

    Create RestaurantList.js

---
+++ Wk05_AaishwaryaLakshmiP/RestaurantList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import RestaurantCard from './RestaurantCard';
+import './App.css';
+
+class RestaurantList extends Component {
+  render() {
+    const { restaurants, onRestaurantClick } = this.props;
+    return (
+      <div className="restaurant-list">
+        {restaurants.map((restaurant, index) => (
+          <RestaurantCard key={index} restaurant={restaurant} onClick={() => onRestaurantClick(restaurant)} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default RestaurantList;
