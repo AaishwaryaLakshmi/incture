8b39cb75f621cd5ffcdcc6f78441d711e660cacb
Aaishwarya Lakshmi <84765376+AaishwaryaLakshmi@users.noreply.github.com>
2024-02-19 07:59:58 +0530

    Create Header.js

---
+++ Wk05_AaishwaryaLakshmiP/Header.js
@@ -0,0 +1,15 @@
+// Header.js
+import React, { Component } from 'react';
+import './App.css'; // Import the corresponding CSS file for styling
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Foodie Mania</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
